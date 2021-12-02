7f9a616f6adeb6e418b34a91c24fb6001faec535
rileydakota <dakotariley2@gmail.com>
2021-12-02 17:02:35 +0000

    feat: add day 2 part 2 solution

--- solutions/day_2/src/main.rs
+++ solutions/day_2/src/main.rs
@@ -1,7 +1,8 @@
 use std::fs;
 
 fn main() {
-    part_1()
+    part_1();
+    part_2();
 }
 
 struct Position {
@@ -43,7 +44,6 @@ fn part_1() {
     let split = problem_data.split("\n");
 
     for direction in split {
-        println!("{}", direction);
         let dir_split: Vec<String> = direction.split_whitespace().map(|s| s.to_string()).collect();
         if dir_split[0] == "forward" {
             let int: i32 = dir_split[1].parse().unwrap();
@@ -56,6 +56,68 @@ fn part_1() {
         else if dir_split[0] == "down" {
             let int: i32 = dir_split[1].parse().unwrap();
             pos.down(int)
+        }
+    }
+    pos.print_current_pos();
+    println!("Answer is {}", pos.depth * pos.horizontal)
+}
+
+
+
+struct PositionAim {
+    depth: i32,
+    horizontal: i32,
+    aim: i32
+}
+
+impl PositionAim {
+
+    fn up_aim(&mut self, amount: i32){
+        self.aim -= amount
+    }
+
+    fn down_aim(&mut self, amount: i32){
+        self.aim += amount
+    }
+
+    fn forward_aim(&mut self, amount: i32){
+        self.horizontal += amount;
+        self.depth += self.aim * amount;
+    }
+
+
+    fn print_current_pos(&self){
+        println!("current depth is {} and current horizontal distance is {}", self.depth, self.horizontal)
+    }
+}
+fn part_2() {
+    println!("Advent of Code 2021 - Day 2 Part 2!");
+    
+    let filename = "../../data/day_2.txt";
+    let load_statement = format!("Loading problem data from {}", filename);
+    
+    let mut pos = PositionAim { depth: 0, horizontal: 0, aim: 0 };
+
+    println!("{}", load_statement);
+
+    let problem_data = fs::read_to_string(filename)
+        .expect("loading of file failed!");
+    
+    let split = problem_data.split("\n");
+
+    for direction in split {
+        let dir_split: Vec<String> = direction.split_whitespace().map(|s| s.to_string()).collect();
+        if dir_split[0] == "forward" {
+            let int: i32 = dir_split[1].parse().unwrap();
+            pos.forward_aim(int);
+        }
+        else if dir_split[0] == "up" {
+            let int: i32 = dir_split[1].parse().unwrap();
+            pos.up_aim(int);
+        }
+        else if dir_split[0] == "down" {
+            let int: i32 = dir_split[1].parse().unwrap();
+            pos.down_aim(int);
         }
     }
     pos.print_current_pos();
